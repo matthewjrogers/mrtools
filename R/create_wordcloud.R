a7f391cd4b8e7978098c71fa533b62074034d095
matthewjrogers <matthew.rogers09@gmail.com>
2022-11-23 07:47:59 -0500

    modify wordcloud func

--- R/create_wordcloud.R
+++ R/create_wordcloud.R
@@ -13,13 +13,13 @@
 #' @import webshot
 create_wordcloud <- function(frequencies, file_name, colors = c('#292f69', '#1396d3', '#7ac143'), shape = "circle", width = 1992, height = 1744){
 
-  wc <- wordcloud2::wordcloud2(frequencies, color = colors, shape = shape)
+  wc <- wordcloud2::wordcloud2(frequencies, color = rep_len(colors, nrow(frequencies)), shape = shape)
 
-  htmlwidgets::saveWidget(wc, "temp_wc.html", selfcontained = FALSE)
+  htmlwidgets::saveWidget(wc, file.path(tempdir(), "temp_wc.html"), selfcontained = FALSE)
 
-  webshot::webshot("temp_wc.html", file_name, vwidth = width, vheight = height, delay = 10)
+  webshot::webshot(file.path(tempdir(), "temp_wc.html"), file_name, vwidth = width, vheight = height, delay = 10)
 
-  unlink('temp_wc.html')
+  unlink(file.path(tempdir(), "temp_wc.html"))
 
   message("Wordcloud created")
 }
